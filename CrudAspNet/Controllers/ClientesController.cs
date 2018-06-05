5e2467167ea5411797b463821ed4da20acbbd361
karencplara <34969669+karencplara@users.noreply.github.com>
2018-06-05 11:00:27 -0300

    Arquivos
    Alterações

---
+++ CrudAspNet/Controllers/ClientesController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CrudAspNet.Models;
+using X.PagedList;
+using AtividadeCrud.Models;
+
+namespace CrudAspNet.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        // GET: Clientes
+
+        public ActionResult Index(string busca = "", int pagina = 1)
+        {
+
+            if (!String.IsNullOrWhiteSpace(busca))
+            {
+                ViewBag.Busca = busca;
+                return View(db.Clientes
+                              .Where(c => c.Nome.Contains(busca))
+                              .OrderBy(c => c.Nome)
+                              .ToPagedList(pagina, 5));
+            }
+            else
+            {
+                return View(db.Clientes
+                             .OrderBy(c => c.Nome)
+                             .ToPagedList(pagina, 5));
+            }
+
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Email,Cpf,Telefone,Endereco")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clientes.Add(cliente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cliente);
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Clientes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Email,Cpf,Telefone,Endereco")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cliente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cliente);
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cliente cliente = db.Clientes.Find(id);
+            db.Clientes.Remove(cliente);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CrudAspNet/Models/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace CrudAspNet.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(200, ErrorMessage = "Esse campo deve ter até 200 caracteres.")]
+        public string Nome { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(11, ErrorMessage = "Esse campo deve ter 11 caracteres.")]
+        public string Cpf { get; set; }
+        [Required]
+        [StringLength(11, ErrorMessage = "Esse campo deve ter até 11 caracteres com DDD")]
+        public string Telefone { get; set; }
+        [Required]
+        public string Endereco { get; set; }
+    }
+}
